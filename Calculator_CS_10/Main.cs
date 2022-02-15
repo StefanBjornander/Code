26d9d768d118cf5b79a489e7d21d5069f3ad37ac
Stefan Bjornander <stefan.bjornander@outlook.com>
2022-02-15 23:31:00 +0100

    Backup

---
+++ Calculator_CS_10/Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Calculator {
+  class MainX {
+    public static Dictionary<string,double> VariableMap =
+      new Dictionary<string,double>();
+          
+    static void Main(string[] args) {
+      if (args.Length != 1) {
+        Console.Out.WriteLine("Usage: calculator inputfile");
+      }
+
+      { System.Globalization.CultureInfo customCulture =
+         (System.Globalization.CultureInfo)
+         System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+        customCulture.NumberFormat.NumberDecimalSeparator = ".";
+        System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+      }
+
+      VariableMap["pi"] = Math.PI;
+      VariableMap["e"] = Math.E;
+
+      string s = "abcd", t = "\"\"";
+      string u = s.Substring(1, s.Length - 2);
+      string v = s.Substring(1, t.Length - 2);
+
+      //string text = Console.In.ReadLine();
+
+      try {
+        FileStream file = new FileStream(args[0], FileMode.Open);
+        Scanner scanner = new Scanner(file);
+        Parser parser = new Parser(scanner);
+        parser.Parse();
+      }
+      catch (Exception exception) {
+        Console.Out.WriteLine(exception.ToString());
+      }
+
+//      Console.In.ReadLine();
+    }
+  }
+
+  public partial class Parser :
+         QUT.Gppg.ShiftReduceParser<ValueType, QUT.Gppg.LexLocation> {
+    public Parser(Scanner scanner)
+     :base(scanner) {
+      // Empty.
+    }
+  }
+}
---
+++ Calculator_CS_10/Parser.bat
@@ -0,0 +1,17 @@
+﻿echo Hello
+
+@C:
+@cd "C:\Users\stefa\Documents\A A C_Compiler_CSharp_10\Calculator_CS_10"
+@"C:\gppg-distro-1_5_2\binaries\Gppg" /gplex Parser.gppg > Parser.cs
+@"C:\gppg-distro-1_5_2\binaries\Gplex" Scanner.gplex
+@rem @C:\YACC -d Parser.bison
+
+@rem cd C:\Users\Stefa\Documents\Calculator_Bison_Flex\Calculator
+@rem "C:\Program Files (x86)\GnuWin32\bin\bison" Parser.bison
+@rem "C:\Program Files (x86)\GnuWin32\bin\flex" Scanner.flex
+
+@rem move y.tab.c Parser.cpp
+@rem move y.tab.h Parser.h
+@rem move lex.yy.c Scanner.cpp
+
+@pause
